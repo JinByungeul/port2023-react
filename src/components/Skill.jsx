b126403e05666a9275195de9934695d46b1e4b31
Jin Byungeul <vbjiny@gmail.com>
2024-10-31 18:25:38 +0900

    😂 스킬영역 완료

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { skillText } from "../constants";
 
 const Skill = () => {
-    return <div>Skill</div>;
+    return (
+        <section id="skill">
+            <div className="skill_inner">
+                <h2 className="skill_title">Challenge <em>나의 도전</em></h2>
+                <div className="skill_desc">
+                    {skillText.map((skill, key) => (
+                        <div>
+                            <span>{key + 1}.</span>
+                            <h3>{skill.title}</h3>
+                            <p>{skill.desc}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Skill;
