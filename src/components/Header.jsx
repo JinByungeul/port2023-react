854457dc662bb8819866ab5f2f09687d15ed807c
Jin Byungeul <vbjiny@gmail.com>
2024-10-31 13:28:13 +0900

    😘 헤더영역 완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+
+const headerNav = [
+    {
+        title: "intro",
+        url: "#intro",
+    },
+    {
+        title: "skill",
+        url: "#skill",
+    },
+    {
+        title: "site",
+        url: "#site",
+    },
+    {
+        title: "portfolio",
+        url: "#portfolio",
+    },
+    {
+        title: "contact",
+        url: "#contact",
+    },
+];
 
 const Header = () => {
-    return <div>Header</div>;
+    // 상태변수 show 생성 및 초기화
+    const [show, setShow] = useState(false);
+
+    const toggleMenu = () => {
+        setShow((prevShow) => !prevShow);
+    };
+
+    return (
+        <header id="header" role="banner">
+            <div className="header_inner">
+                <div className="header_logo">
+                    <a href="/">portfolio <em>vite</em></a>
+                </div>
+                <nav className={`header_nav ${show ? "show" : ""}`} role="navigation" aria-label="메인 메뉴">
+                    <ul>
+                        {headerNav.map((nav, key) => (
+                            <li key={key}><a href={nav.url}>{nav.title}</a></li>
+                        ))}
+                    </ul>
+                </nav>
+                <div
+                    className="header_nav_mobile"
+                    id="headerToggle"
+                    aria-controls="primary-menu"
+                    aria-expanded="false"
+                    role="button"
+                    tabIndex="0"
+                    onClick={toggleMenu}
+                >
+                    <span></span>
+                </div>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Intro from "../components/Intro";
 import Main from "../components/Main";
 import Port from "../components/Port";
 import Site from "../components/Site";
